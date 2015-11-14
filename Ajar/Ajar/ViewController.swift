9e6ed8938249cb36bc6c277f849ad52793325dc7
Catherine Borsting <cBorsting@rescomp-15-264639.stanford.edu>
2015-11-13 23:25:24 -0800

    Made Project

---
+++ Ajar/Ajar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ajar
+//
+//  Created by Catherine Borsting on 11/13/15.
+//  Copyright (c) 2015 Catherine Borsting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
