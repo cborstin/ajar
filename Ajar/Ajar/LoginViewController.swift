722f6d8ca7ee562282e447ff85ac697f19bba942
Catherine Borsting <cBorsting@rescomp-15-264639.stanford.edu>
2015-11-18 21:44:39 -0800

    Added Stuff

---
+++ Ajar/Ajar/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  Ajar
+//
+//  Created by Catherine Borsting on 11/18/15.
+//  Copyright (c) 2015 Catherine Borsting. All rights reserved.
+//
+
+import Cocoa
+
+class LoginViewController: UIViewController {
+
+}
